f686dec8c57c7ad14fe675f147e549ce5a6de4db
supachai1998 <61734655+supachai1998@users.noreply.github.com>
2020-11-30 10:01:05 +0700

    sendall
    ลองใช้โค๊ดนี้ดู

--- importsocket.py
+++ importsocket.py
@@ -1,7 +1,11 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("djxmmx.net", 17))
-message = b''
-s.sendall(message)
-data, address = s.recvfrom(1024)
-print(data.decode())
+while  True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(("webcode.me" , 80))
+        s.sendall(b"GET / HTTP/1.1\r\nHost: webcode.me\r\nAccept: text/html\r\nConnection: close\r\n\r\n")
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            print(data.decode())
+    time.sleep(5)
+
